08bb4ee869f79eae74c8fbf589242d97e248e489
Ricardo González <ricardo@ricardogonz.es>
2023-12-07 02:57:17 +0100

    Starting doing backend babyy, time 2 go 2 sleep

--- www/static/post.js
+++ www/static/post.js
@@ -1,8 +1,27 @@
 /*
     == commentarius | 2 words: blog, simple ==
 
-    SOURCE CODE VERSION 0.0
+    SOURCE CODE VERSION 1.0
 
     Author: Ricardo González @ RESIB Labs ( www.ricardogonz.es - resiblabs.taniwa.es )
     License: GPL 3.0
 */
+
+function readgGet(name){
+    if(name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(location.search))
+       return decodeURIComponent(name[1]);
+} // Thank you to Rafael on stackoverflow for this tool!
+
+// Define user input
+const postName = readgGet("p");
+const postFile = postName + ".md";
+
+// Get post data
+
+
+// Convert post data
+
+// Show post to user
+document.getElementById("post-post-title").innerHTML = postName;
+document.title += " - " + postName;
+document.getElementById("post-post-content").innerHTML = postContent;
