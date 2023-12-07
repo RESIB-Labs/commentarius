cfcfea288cc017ee91c07a31d1ebdf32e59f3d75
Ricardo González <ricardo@ricardogonz.es>
2023-12-07 02:10:49 +0100

    Updated more things

--- www/static/post.js
+++ www/static/post.js
@@ -1,5 +1,8 @@
-# commentarius | 2 words: blog, simple
+/*
+    == commentarius | 2 words: blog, simple ==
+
     SOURCE CODE VERSION 0.0
 
     Author: Ricardo González @ RESIB Labs ( www.ricardogonz.es - resiblabs.taniwa.es )
     License: GPL 3.0
+*/
